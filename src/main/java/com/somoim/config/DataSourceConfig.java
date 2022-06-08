30ae3cf5dd94a002938f8fc629072217c04fa5de
wj-dominic <ywj8909@gmail.com>
2022-06-09 02:47:56 +0900

    multi datasource 적용하여 readonly일 때, secondary db를 조회하도록 처리

---
+++ src/main/java/com/somoim/config/DataSourceConfig.java
@@ -0,0 +1,86 @@
+package com.somoim.config;
+
+import com.somoim.enumeration.DataSourceType;
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.primary")
+    public DataSourceProperties primaryDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.secondary")
+    public DataSourceProperties secondaryDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource primaryDataSource() {
+        return primaryDataSourceProperties()
+            .initializeDataSourceBuilder()
+            .build();
+    }
+
+    @Bean
+    public DataSource secondaryDataSource() {
+        return secondaryDataSourceProperties()
+            .initializeDataSourceBuilder()
+            .build();
+    }
+
+    @Bean
+    public DataSource routingDataSource(
+        @Qualifier(value = "primaryDataSource") DataSource primaryDataSource,
+        @Qualifier(value = "secondaryDataSource") DataSource secondaryDataSource) {
+
+        AbstractRoutingDataSource routingDataSource = new AbstractRoutingDataSource() {
+            @Override
+            protected Object determineCurrentLookupKey() {
+                return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+                    ? DataSourceType.SECONDARY : DataSourceType.PRIMARY;
+            }
+        };
+
+        Map<Object, Object> targetDataSources = new HashMap<>();
+
+        targetDataSources.put(DataSourceType.PRIMARY, primaryDataSource);
+        targetDataSources.put(DataSourceType.SECONDARY, secondaryDataSource);
+
+        routingDataSource.setTargetDataSources(targetDataSources);
+        routingDataSource.setDefaultTargetDataSource(primaryDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean
+    public DataSource lazyRoutingDataSource(
+        @Qualifier(value = "routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(
+        @Qualifier(value = "lazyRoutingDataSource") DataSource lazyRoutingDataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(lazyRoutingDataSource);
+        return transactionManager;
+    }
+}
