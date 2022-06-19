4b9953927c18b2915387c8a603f2206966c6fb35
wj-dominic <ywj8909@gmail.com>
2022-06-19 19:44:47 +0900

    User dao @Builder를 클래스 대상으로 변경, gender의 타입을 enumeration으로 변경

--- src/main/java/com/somoim/model/dao/User.java
+++ src/main/java/com/somoim/model/dao/User.java
@@ -1,17 +1,17 @@
 package com.somoim.model.dao;
 
+import com.somoim.enumeration.GenderType;
 import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class User {
+
     @NotBlank
     private Long id;
     @Email
@@ -21,7 +21,7 @@ public class User {
     private String password;
     private String name;
     private String birth;
-    private String gender;
+    private GenderType gender;
     private Integer cityCode1;
     private Integer cityCode2;
     private Long imageId;
@@ -29,19 +29,4 @@ public class User {
     private LocalDateTime modifyAt;
     private Boolean disband;
 
-    @Builder(builderMethodName = "signUpUser", builderClassName = "signUpUser")
-    public User(String email, String password, LocalDateTime createAt, LocalDateTime modifyAt) {
-        this.email = email;
-        this.password = password;
-        this.createAt = createAt;
-        this.modifyAt = modifyAt;
-        this.disband = false;
-    }
-
-    @Builder(builderMethodName = "resignUser", builderClassName = "resignUser")
-    public User(String email, LocalDateTime modifyAt) {
-        this.email = email;
-        this.modifyAt = modifyAt;
-        this.disband = true;
-    }
 }
